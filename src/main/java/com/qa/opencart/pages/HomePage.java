a3f66e2750361ceb555ee3c726464eba89221d9a
aravind-rupeek <aravind.muthukrishnan@rupeek.com>
2021-10-03 20:15:48 +0530

    HomePage class added & gitIgnore modified

---
+++ src/main/java/com/qa/opencart/pages/HomePage.java
@@ -0,0 +1,44 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+	private WebDriver driver;
+	private By myAccountDropDown = By.xpath("//span[@class='caret']");
+	private By myAccountLink = By.linkText("My Account");
+	private By myAccountLabel = By.xpath("//h2[text()='My Account']");
+	private By myOrdersLabel = By.xpath("//h2[text()='My Orders']");
+	
+	public HomePage(WebDriver driver) {
+		this.driver=driver;
+	}
+	
+	public String getHomePageTitle() {
+		return driver.getTitle();
+	}
+	
+	public boolean isMyAccountLinkPresent() {
+		driver.findElement(myAccountDropDown).click();
+		if (driver.findElement(myAccountLink).isDisplayed()) {
+			return true;   	
+		}
+		return false;
+		
+	}
+	
+	public boolean isMyAccountLabelPresent() {
+		if (driver.findElement(myAccountLabel).isDisplayed()) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isMyOrdersLabelPresent() {
+		if (driver.findElement(myOrdersLabel).isDisplayed()) {
+			return true;
+		}
+		return false;
+	}
+}
