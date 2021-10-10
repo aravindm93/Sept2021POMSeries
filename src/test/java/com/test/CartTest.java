e6843646b0c92022992c88f3ad553773463835b5
aravindm93 <m_arvind93@yahoo.com>
2021-10-10 21:40:09 +0530

    Cart Page class and Test class added

---
+++ src/test/java/com/test/CartTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CartTest {
+	
+	@Test
+	public void getCartTest() {
+		Assert.assertEquals(true, true);
+	}
+
+}
