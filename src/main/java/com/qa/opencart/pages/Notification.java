a3ee111194001a03aa7e4c537727f9479991ad9d
aravindm93 <m_arvind93@yahoo.com>
2021-11-01 19:51:46 +0530

    Notification class changes updated

--- src/main/java/com/qa/opencart/pages/Notification.java
+++ src/main/java/com/qa/opencart/pages/Notification.java
@@ -3,18 +3,11 @@ package com.qa.opencart.pages;
 public class Notification {
 	
 	public void getNotification() {
-<<<<<<< Updated upstream
 
-		System.out.println("Notification Logic changed");
 		System.out.println("Notification Logic---Aravind M");
-=======
-<<<<<<< HEAD
+
 		System.out.println("Notification Logic---Naveen");
->>>>>>> Stashed changes
-		
-=======
-		System.out.println("Notification Logic---Aravind");
->>>>>>> 1ad3fb2236faf5d15b947e69626313e9b6afeaee
+
 	}
 		public void sendNotification() {
 			System.out.println("Notification successfully sent");
