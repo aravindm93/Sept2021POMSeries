7edd1469adaba92be739f970a468bd4a92206cd9
Aravind M <60823438+aravindm93@users.noreply.github.com>
2021-11-01 19:54:43 +0530

    Update Notification.java

--- src/main/java/com/qa/opencart/pages/Notification.java
+++ src/main/java/com/qa/opencart/pages/Notification.java
@@ -4,7 +4,7 @@ public class Notification {
 	
 	public void getNotification() {
 
-		System.out.println("Notification Logic -- Amit");
+		System.out.println("Notification Logic 2 -- Amit Sharma");
 
 		System.out.println("Notification Logic---Naveen");
 
